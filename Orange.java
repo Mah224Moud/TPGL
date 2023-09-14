f2a0a81e35edb6ce549ff8de0305788aeabf69bd
Mamoudou DIALLO <mamoudoudiallo2397@gmail.com>
2023-09-14 10:54:48 +0200

    First commit ==> Added Orange classe with some test in the main function

---
+++ Orange.java
@@ -0,0 +1,73 @@
+/**
+ *
+ * @author roudet
+ */
+public class Orange {
+    private double prix;
+    private String origine;
+	
+    public Orange() 
+    {
+        this.prix = 0.5;  //prix en euros
+        this.origine="Espagne";
+    }
+    
+    public Orange(double prix, String origine) 
+    {
+	if(prix < 0)
+	    this.prix = -prix;  //une solution possible pour interdire les prix negatifs
+	else
+	    this.prix = prix;
+
+	if(origine.equals(""))
+            this.origine = "Espagne";  //Espagne par défaut
+	else
+            this.origine = origine;   
+    }
+
+    public double getPrix(){
+	return prix;
+    }
+
+    public void setPrix(double prix){
+	this.prix=prix;
+    }
+
+    public String getOrigine(){
+	return origine;
+    }
+ 
+    public void setOrigine(String origine){
+	this.origine=origine;
+    }
+
+    @Override
+    public String toString(){
+        return "Orange de " + origine + " a " + prix + " euros";
+    }
+
+    @Override
+    public boolean equals(Object o){  //predicat pour tester si 2 oranges sont equivalentes
+        if(o != null && getClass() == o.getClass()){
+            Orange or = (Orange) o;
+            return (prix == or.prix && origine.equals(or.origine));
+        }
+        return false;
+    }
+
+    public boolean isSeedless() {  //predicat indiquant qu'une orange a des pepins
+        return false;
+    }
+
+
+    public static void main (String[] args){
+        
+        Orange orange = new Orange();
+        Orange orangeParams = new Orange(158, "Danemark");
+        Orange orangeNegatif = new Orange(-3, "Allemagne");
+
+	    System.out.println("Orange avec le const vide: " + orange);
+        System.out.println("Orange avec les param initialisé: "+ orangeParams);
+        System.out.println("Orange avec le prix negatif: " + orangeNegatif);
+   }
+}
